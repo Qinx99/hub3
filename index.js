8b477ddfa5cfcfd509ee882d5ee068a901507667
Qinx99 <144615830+Qinx99@users.noreply.github.com>
2023-11-07 20:51:59 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,186 @@
+var game = document.getElementById('games');
+
+function selectGame() {
+  if (game.value === ' ') {
+    alert('Pleas Choose A Game!');
+  } else if (game.value === 'abduction') {
+    window.location.href = 'runners/abduction.html';
+  } else if (game.value === 'achievmentunlocked') {
+    window.location.href = 'runners/achievmentunlocked.html';
+  } else if (game.value === 'achievmentunlocked2') {
+    window.location.href = 'runners/achievmentunlocked2.html';
+  } else if (game.value === 'ageofwar') {
+    window.location.href = 'runners/ageofwar.html';
+  } else if (game.value === 'ageofwar2') {
+    window.location.href = 'runners/ageofwar2.html';
+  } else if (game.value === 'appleshooter') {
+    window.location.href = 'runners/appleshooter.html';
+  } else if (game.value === 'arcs') {
+    window.location.href = 'runners/arcs.html';
+  } else if (game.value === 'armormayhem') {
+    window.location.href = 'runners/armormayhem.html';
+  } else if (game.value === 'badpiggies') {
+    window.location.href = 'runners/badpiggies.html';
+  } else if (game.value === 'battleships') {
+    window.location.href = 'runners/battleships.html';
+  } else if (game.value === 'beatupyourpc') {
+    window.location.href = 'runners/beatupyourpc.html';
+  } else if (game.value === 'bigredbutton') {
+    window.location.href = 'runners/bigredbutton.html';
+  } else if (game.value === 'bloonsplayerpack') {
+    window.location.href = 'runners/bloonsplayerpack.html';
+  } else if (game.value === 'bloonsplayerpack2') {
+    window.location.href = 'runners/bloonsplayerpack2.html';
+  } else if (game.value === 'bloonsplayerpack3') {
+    window.location.href = 'runners/bloonsplayerpack3.html';
+  } else if (game.value === 'bloonsplayerpack4') {
+    window.location = 'runners/bloonsplayerpack4.html';
+  } else if (game.value === 'bloonspop3') {
+    window.location = 'runners/bloonspop3.html';
+  } else if (game.value === 'bloonssupermonkey') {
+    window.location.href = 'runners/bloonssupermonkey.html';
+  } else if (game.value === 'bloonstowerdefense') {
+    window.location.href = 'runners/bloonstowerdefense.html';
+  } else if (game.value === 'bloonstowerdefense2') {
+    window.location.href = 'runners/bloonstowerdefense2.html';
+  } else if (game.value === 'bloonstowerdefense3') {
+    window.location.href = 'runners/bloonstowerdefense3.html';
+  } else if (game.value === 'bloonstowerdefense4') {
+    window.location.href = 'runners/bloonstowerdefense4.html';
+  } else if (game.value === 'bloonstowerdefense4expansion') {
+    window.location.href = 'runners/bloonstowerdefense4expansion.html';
+  } else if (game.value === 'bloxorz') {
+    window.location.href = 'runners/bloxorz.html';
+  } else if (game.value === 'boombot') {
+    window.location.href = 'runners/boombot.html';
+  } else if (game.value === 'boombot2') {
+    window.location.href = 'runners/boombot2.html';
+  } else if (game.value === 'breakingintothebank') {
+    window.location.href = 'runners/breakingintothebank.html';
+  } else if (game.value === 'bridgetactics') {
+    window.location.href = 'runners/bridgetactics.html';
+  } else if (game.value === 'buildingdemolisher') {
+    window.location.href = 'runners/buildingdemolisher.html';
+  } else if (game.value === 'canyondefense') {
+    window.location.href = 'runners/canyondefense.html';
+  } else if (game.value === 'catmario') {
+    window.location.href = 'runners/catmario.html';
+  } else if (game.value === 'causalitycamp') {
+    window.location.href = 'runners/causalitycamp.html';
+  } else if (game.value === 'causalityfestival') {
+    window.location.href = 'runners/causalityfestival.html';
+  } else if (game.value === 'causalitykitchen') {
+    window.location.href = 'runners/causalitykitchen.html';
+  } else if (game.value === 'caveman') {
+    window.location.href = 'runners/caveman.html';
+  } else if (game.value === 'chibikinght') {
+    window.location.href = 'runners/chibikinght.html';
+  } else if (game.value === 'chibikinghthacked') {
+    window.location.href = 'runners/chibikinghthacked.html';
+  } else if (game.value === 'chinesecheckers') {
+    window.location.href = 'runners/chinesecheckers.html';
+  } else if (game.value === 'clickdeathbowling') {
+    window.location.href = 'runners/clickdeathbowling.html';
+  } else if (game.value === 'coverorange') {
+    window.location.href = 'runners/coverorange.html';
+  } else if (game.value === 'coverorangejourneypirates') {
+    window.location.href = 'runners/coverorangejourneypirates.html';
+  } else if (game.value === 'coverorangeknights') {
+    window.location.href = 'runners/coverorangeknights.html';
+  } else if (game.value === 'coverorangewildwest') {
+    window.location.href = 'runners/coverorangewildwest.html';
+  } else if (game.value === 'crushthecastle2') {
+    window.location.href = 'runners/crushthecastle2.html';
+  } else if (game.value === 'cubedroidsavesthecalaxy') {
+    window.location.href = 'runners/cubedroidsavesthecalaxy.html';
+  } else if (game.value === 'cubefield') {
+    window.location.href = 'runners/cubefield.html';
+  } else if (game.value === 'cursorinvisible') {
+    window.location.href = 'runners/cursorinvisible.html';
+  } else if (game.value === 'dacep27') {
+    window.location.href = 'runners/dacep27.html';
+  } else if (game.value === 'deadparadise') {
+    window.location.href = 'runners/deadparadise.html';
+  } else if (game.value === 'doodlegod') {
+    window.location.href = 'runners/doodlegod.html';
+  } else if (game.value === 'duckhunt') {
+    window.location.href = 'runners/duckhunt.html';
+  } else if (game.value === 'ducklife1') {
+    window.location.href = 'runners/ducklife1.html';
+  } else if (game.value === 'ducklife2') {
+    window.location.href = 'runners/ducklife2.html';
+  } else if (game.value === 'ducklife2hacked') {
+    window.location.href = 'runners/ducklife2hacked.html';
+  } else if (game.value === 'ducklife3') {
+    window.location.href = 'runners/ducklife3.html';
+  } else if (game.value === 'ducklife3hacked') {
+    window.location.href = 'runners/ducklife3hacked.html';
+  } else if (game.value === 'ducklife4') {
+    window.location.href = 'runners/ducklife4.html';
+  } else if (game.value === 'ducklife4hacked') {
+    window.location.href = 'runners/ducklife4hacked.html';
+  } else if (game.value === 'dumpertruck') {
+    window.location.href = 'runners/dumpertruck.html';
+  } else if (game.value === 'earntodie') {
+    window.location.href = 'runners/earntodie.html';
+  } else if (game.value === 'earntodie2012part2') {
+    window.location.href = 'runners/earntodie2012part2.html';
+  } else if (game.value === 'electricman2') {
+    window.location.href = 'runners/electricman2.html';
+  } else if (game.value === 'electricman2hacked') {
+    window.location.href = 'runners/electricman2hacked.html';
+  } else if (game.value === 'endoftheworld') {
+    window.location.href = 'runners/endoftheworld.html';
+  } else if (game.value === 'escapingtheprison') {
+    window.location.href = 'runners/escapingtheprison.html';
+  } else if (game.value === 'fancypantsworld1') {
+    window.location.href = 'runners/fancypantsworld1.html';
+  } else if (game.value === 'fancypantsworld2') {
+    window.location.href = 'runners/fancypantsworld2.html';
+  } else if (game.value === 'feedus1') {
+    window.location.href = 'runners/feedus1.html';
+  } else if (game.value === 'feedus2') {
+    window.location.href = 'runners/feedus2.html';
+  } else if (game.value === 'feedus3') {
+    window.location.href = 'runners/feedus3.html';
+  } else if (game.value === 'feedus4') {
+    window.location.href = 'runners/feedus4.html';
+  } else if (game.value === 'feedus5') {
+    window.location.href = 'runners/feedus5.html';
+  } else if (game.value === 'feedushappy') {
+    window.location.href = 'runners/feedushappy.html';
+  } else if (game.value === 'feeduslostisland') {
+    window.location.href = 'runners/feeduslostisland.html';
+  } else if (game.value === 'feeduspirates') {
+    window.location.href = 'runners/feeduspirates.html';
+  } else if (game.value === 'feedusxmasexpansion') {
+    window.location.href = 'runners/feedusxmasexpansion.html';
+  } else if (game.value === 'fireboyandwatergirl1') {
+    window.location.href = 'runners/fireboyandwatergirl1.html';
+  } else if (game.value === 'flappybird') {
+    window.location.href = 'runners/flappybird.html';
+  } else if (game.value === 'fleeingthecomplex') {
+    window.location.href = 'runners/fleeingthecomplex.html';
+  } else if (game.value === 'flight') {
+    window.location.href = 'runners/flight.html';
+  } else if (game.value === 'flingecommerce') {
+    window.location.href = 'runners/flingecommerce.html';
+  } else if (game.value === 'fostershomeforimaginaryfriendswiltswashnswoosh') {
+    window.location.href =
+      'runners/fostershomeforimaginaryfriendswiltswashnswoosh.html';
+  } else if (game.value === 'fragger') {
+    window.location.href = 'runners/fragger.html';
+  } else if (game.value === 'giveup') {
+    window.location.href = 'runners/giveup.html';
+  } else if (game.value === 'giveup2') {
+    window.location.href = 'runners/giveup2.html';
+  } else if (game.value === 'goldminer') {
+    window.location.href = 'runners/goldminer.html';
+  } else if (game.value === 'goldminerhacked') {
+    window.location.href = 'runners/goldminerhacked.html';
+  } else if (game.value === 'goldminerspecialedition') {
+    window.location.href = 'runners/goldminerspecialedition.html';
+  } else if (game.value === 'gravityduckhacked') {
+    window.location.href = 'runners/gravityduckhacked.html';
+  }
+}
